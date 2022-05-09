e2ab5e13dd09e6b65591b1d9e924eb248eddeab9
Capwonderbread <hectcast10@yahoo.com>
2022-05-08 22:30:06 -0700

    added heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoute');
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
